38070b115829d538a56a4cea1a4e2108d5c7a23c
Alex Whan <alexwhan@gmail.com>
2018-11-01 14:48:19 +1100

    Initial commit

---
+++ R/basic_mean.R
@@ -0,0 +1,14 @@
+#' A basic mean calculation
+#'
+#' @param vec A numeric vector
+#'
+#' @return A double
+#' @export
+#'
+#' @examples
+#' basic_mean(1:3)
+basic_mean <- function(vec) {
+  vec_sum <- sum(vec)
+  n <- length(vec)
+  return(vec_sum / n)
+}
