0c9c804e6cce5630327853f95044036b6fdb6641
Alex Whan <alexwhan@gmail.com>
2018-11-01 15:31:05 +1100

    added tests and make_tibble

--- R/basic_mean.R
+++ R/basic_mean.R
@@ -8,7 +8,20 @@
 #' @examples
 #' basic_mean(1:3)
 basic_mean <- function(vec) {
+  if(!is.numeric(vec)) stop("vec must be numeric")
+
   vec_sum <- sum(vec)
   n <- length(vec)
   return(vec_sum / n)
+}
+
+#' Silly
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples
+#' make_tibble()
+make_tibble <- function() {
+  tibble::data_frame(a = 1)
 }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(alextest)
+
+test_check("alextest")
